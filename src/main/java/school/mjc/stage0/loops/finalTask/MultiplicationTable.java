bd16d2ac0915325ef33f623577cec8f806238b7b
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-04-05 14:17:04 +0500

    Bismillah 🥇

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public void printTable(int numberTableToPrint) {
+        if (numberTableToPrint <= 0) {
+            System.out.println("number is not positive");
+        } else {
+            for (int i = 1; i < 11; i++) {
+                System.out.println(numberTableToPrint + " x " + i + " = " + numberTableToPrint * i);
+            }
+        }
     }
 }
