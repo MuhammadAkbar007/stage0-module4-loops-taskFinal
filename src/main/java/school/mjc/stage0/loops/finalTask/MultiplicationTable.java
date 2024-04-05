82ce4ac601846fc48f865f252446bf7b957d8cf4
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-04-05 16:32:00 +0500

    start with zero 0️⃣

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint) {
-        if (numberTableToPrint <= 0) {
+        if (numberTableToPrint < 0) {
             System.out.println("number is not positive");
         } else {
             for (int i = 1; i < 11; i++) {
-                System.out.println(numberTableToPrint + " x " + i + " = " + numberTableToPrint * i);
+                System.out.println(i + " x " + numberTableToPrint + " = " + numberTableToPrint * i);
             }
         }
     }
