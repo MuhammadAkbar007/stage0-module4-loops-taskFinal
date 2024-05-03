e243b95b44810f5aa613361146bf4fb9b09b64dd
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-05-03 11:17:52 +0500

    unnecessary import 🖨

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,15 +1,13 @@
 package school.mjc.stage0.loops.finalTask;
 
-import java.util.Scanner;
-
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint) {
-        if (numberTableToPrint < 0) {
-            System.out.println("number is not positive");
-        } else {
-            for (int i = 1; i < 11; i++) {
-                System.out.println(i + " x " + numberTableToPrint + " = " + numberTableToPrint * i);
-            }
-        }
-    }
+	public void printTable(int numberTableToPrint) {
+		if (numberTableToPrint < 0) {
+			System.out.println("number is not positive");
+		} else {
+			for (int i = 1; i < 11; i++) {
+				System.out.println(i + " x " + numberTableToPrint + " = " + numberTableToPrint * i);
+			}
+		}
+	}
 }
